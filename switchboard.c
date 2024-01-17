570eca9ef9505f8c08d78afb9ae5b7768ca6fbf4
Matthew Thiga <matthewthiga72@gmail.com>
2024-01-17 05:14:41 -0800

    Added return(0) to nop case

--- switchboard.c
+++ switchboard.c
@@ -25,7 +25,7 @@ int switchboard(char *opcode, int line_num, stack_t **stack, char *par)
 	while (counter < 6)
 	{
 		if (strcmp("nop", opcode) == 0)
-			;
+			return (0);
 		else if (strcmp("push", opcode) == 0)
 		{
 			push(stack, line_num, par);
